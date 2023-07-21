f0bef79dbdc262718dea82ff44b2703bada2d416
hiezen <139339530+hiezen@users.noreply.github.com>
2023-07-20 23:51:21 -0700

    1 file modified, 15 added, 16 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,17 @@ const App = () => {
 
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary"></div>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+        </div>
+    </div>
+    <About />
+    <Experience />
+    <Tech />
+    <Works />
+    <Feedbacks />
     </BrowserRouter>
   )
 }
